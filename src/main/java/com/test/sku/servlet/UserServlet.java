64655b8aa1b9ac696cbb843460c438468d6a4425
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-08-07 16:03:07 +0900

    Update UserServlet.java

--- src/main/java/com/test/sku/servlet/UserServlet.java
+++ src/main/java/com/test/sku/servlet/UserServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet("/user")
@@ -47,9 +48,23 @@ public class UserServlet extends HttpServlet {
             request.setAttribute("detail", userDetail);
             viewPath = "/jsp/userDetailList.jsp";
         }
+				
+		else if(cmd.equals("edit")) {
+	         String uid = request.getParameter("uid");
+	         request.setAttribute("uid", uid);
+	         viewPath = "/jsp/editPwd.jsp";
+	      } else if(cmd.equals("updatePwd")) {
+	         String uid = request.getParameter("uid");
+	         String newPwd = request.getParameter("pwd");
+	         UserDAO dao = new UserDAO();
+	         boolean updated = dao.updatePwd(new User(uid, newPwd));
+	         PrintWriter out = response.getWriter();
+	         out.print("{\"updated\":" + updated + "}");
+	         out.flush();
+	      }
 		
 		if(viewPath!=null) {
-			getServletContext().getRequestDispatcher(viewPath).forward(request, response);	
+			getServletContext().getRequestDispatcher(viewPath).forward(request, response);
 		}
 	}
 }
